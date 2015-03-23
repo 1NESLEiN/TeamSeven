1126ff276321cb3c81488117ed960bd1d80a0cce
ThorPedersen <technostamper@hotmail.com>
2015-03-23 17:51:57 +0100

    Tilføjet menu og User controls i stedet for windows.

---
+++ AddDocumentationUserControl.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Control;
+
+namespace DevelopmentProject
+{
+   /// <summary>
+   /// Interaction logic for AddDocumentationUserControl.xaml
+   /// </summary>
+   public partial class AddDocumentationUserControl : UserControl
+   {
+      private Handler _handler;
+      private DataTable _typesTable;
+      private DataTable _supportersTable;
+      public AddDocumentationUserControl()
+      {
+         InitializeComponent();
+         _handler = Handler.GetInstance();
+
+         PrepareDropBoxes();
+      }
+
+      private void AddDocumentation_OnClick(object sender, RoutedEventArgs e)
+      {
+         try
+         {
+            bool success = _handler.AddDocumentation(Convert.ToInt32(ComboBoxType.SelectedValue), TextBoxHeadline.Text, TextBoxDescription.Text, DatePickerDateCompleted.DisplayDate, Convert.ToInt32(TextBoxTimeSpent.Text), Convert.ToInt32(ComboBoxSupporter.SelectedValue));
+            if (success)
+            {
+               MessageBox.Show("Documentation added succesfully");
+               ClearTextboxes();
+            }
+         }
+         catch (Exception ex)
+         {
+            MessageBox.Show(ex.Message);
+         }
+      }
+
+      private void ClearTextboxes()
+      {
+         TextBoxHeadline.Clear();
+         TextBoxDescription.Clear();
+         TextBoxTimeSpent.Clear();
+         DatePickerDateCompleted.SelectedDate = null;
+         ComboBoxSupporter.SelectedValue = null;
+         ComboBoxType.SelectedValue = null;
+      }
+
+      private void PrepareDropBoxes()
+      {
+         //Get data and populate ComboBoxType
+         _typesTable = _handler.GetTypesTable();
+         ComboBoxType.ItemsSource = _typesTable.DefaultView;
+         ComboBoxType.DisplayMemberPath = "Name";
+         ComboBoxType.SelectedValuePath = "ID";
+
+         //Get data Populate ComboBoxSupporter
+         _supportersTable = _handler.GetSupportersTable();
+         ComboBoxSupporter.ItemsSource = _supportersTable.DefaultView;
+         ComboBoxSupporter.DisplayMemberPath = "Name";
+         ComboBoxSupporter.SelectedValuePath = "ID";
+      }
+   }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -7,16 +7,38 @@ namespace DevelopmentProject
     /// </summary>
     public partial class MainWindow : Window
     {
-        private void BtnAddSupporter_OnClick(object sender, RoutedEventArgs e)
-        {
-            AddSupporterWindow addSupporterWindow = new AddSupporterWindow();
-            addSupporterWindow.Show();
-        }
+        //private void BtnAddSupporter_OnClick(object sender, RoutedEventArgs e)
+        //{
+        //    AddSupporterWindow addSupporterWindow = new AddSupporterWindow();
+        //    addSupporterWindow.Show();
+        //}
 
-        private void BtnAddDocumentation_OnClick(object sender, RoutedEventArgs e)
-        {
-            AddDocumentationWindow addDocumentationWindow = new AddDocumentationWindow();
-            addDocumentationWindow.Show();
-        }
+        //private void BtnAddDocumentation_OnClick(object sender, RoutedEventArgs e)
+        //{
+        //    AddDocumentationWindow addDocumentationWindow = new AddDocumentationWindow();
+        //    addDocumentationWindow.Show();
+        //}
+
+       private void SupporterNew_OnClick(object sender, RoutedEventArgs e)
+       {
+          //AddSupporterWindow addSupporterWindow = new AddSupporterWindow();
+          //addSupporterWindow.Show();
+
+          UserControlPanel.Children.Clear();
+
+          AddSupporterUserControl AddSupporter = new AddSupporterUserControl();
+          UserControlPanel.Children.Add(AddSupporter);
+       }
+       private void DocumentationNew_OnClick(object sender, RoutedEventArgs e)
+       {
+          UserControlPanel.Children.Clear();
+
+          AddDocumentationUserControl addDocumentation = new AddDocumentationUserControl();
+          UserControlPanel.Children.Add(addDocumentation);
+       }
+       private void DocumentationSearch_OnClick(object sender, RoutedEventArgs e)
+       {
+          MessageBox.Show("Documentation Search user Control here");
+       }
     }
 }
