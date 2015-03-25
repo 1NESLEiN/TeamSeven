d02d0af4baedb6b84187dde43da8994f1265f321
ThorPedersen <technostamper@hotmail.com>
2015-03-25 23:23:49 +0100

    Untouched date display changed in search
    Made it so that it doesn't show today date as a default date, but rather
    'select a date'.

--- Model/DBHandler.cs
+++ Model/DBHandler.cs
@@ -195,54 +195,62 @@ namespace Control
 
         public DataTable GetFilteredDocumentationsTable(string keyword, DateTime startDate, DateTime endDate, int supporterID, int typeID)
         {
-            string query =  "SELECT JobDocumentations.ID, Headline, Description, DateCreated, DateCompleted, TimeSpent, Supporters.Name AS SupporterName, Initials, Types.Name AS TypeName FROM dbo.JobDocumentations JOIN dbo.Supporters ON dbo.Supporters.ID = dbo.JobDocumentations.Supporter JOIN dbo.Types ON dbo.Types.ID = dbo.JobDocumentations.Type";
-            if (keyword != "" || !startDate.Equals(DateTime.Now.Date) || !endDate.Equals(DateTime.Now.Date) || supporterID != 0 || typeID != 0)
-            {
-                int counter = 0;
-                query += " WHERE ";
-                if (keyword != "")
-                {
-                    counter++;
+           string query = "SELECT JobDocumentations.ID, Headline, Description, DateCreated, DateCompleted, TimeSpent, Supporters.Name AS SupporterName, Initials, Types.Name AS TypeName FROM dbo.JobDocumentations JOIN dbo.Supporters ON dbo.Supporters.ID = dbo.JobDocumentations.Supporter JOIN dbo.Types ON dbo.Types.ID = dbo.JobDocumentations.Type";
+           if (keyword != "" || !startDate.Equals(DateTime.MinValue.Date) || !endDate.Equals(DateTime.MinValue.Date) || supporterID != 0 || typeID != 0)
+           {
+              int counter = 0;
+              query += " WHERE ";
+              if (keyword != "")
+              {
+                 counter++;
+                 query += "Headline LIKE @Keyword";
+              }
+              if (endDate.Equals(DateTime.MinValue.Date))
+              {
+                 endDate = DateTime.MaxValue.Date;
+              }
+              if (startDate.Equals(DateTime.MinValue.Date))
+              {
+                 //For some reason SQL doesn't like that MinValue date is a very low number, makes an out of range error. So it is added up by 2000 years here.
+                 startDate = DateTime.MinValue.Date.AddYears(2000);
+              }
+              if (!startDate.Equals(DateTime.MinValue.Date) || !endDate.Equals(DateTime.MinValue.Date))
+              {
+                 if (counter > 0) query += " AND ";
+                 counter++;
 
-                    query += "Headline LIKE @Keyword";
-                }
-               if(!startDate.Equals(DateTime.Now.Date) || !endDate.Equals(DateTime.Now.Date))
-                {
-                    if (counter > 0) query += " AND ";
-                    counter++;
+                 query += "DateCreated BETWEEN '" + startDate.ToString("MM/dd/yyyy") + "' AND '" + endDate.ToString("MM/dd/yyyy") + "'";
+              }
+              if (supporterID != 0)
+              {
+                 if (counter > 0) query += " AND ";
+                 counter++;
+                 query += "Supporters.ID = " + supporterID;
+              }
+              if (typeID != 0)
+              {
+                 if (counter > 0) query += " AND ";
+                 counter++;
+                 query += "Types.ID = " + typeID;
+              }
+           }
+           using (SqlConnection sqlConn = new SqlConnection(ConnectionString))
+           using (SqlCommand cmd = new SqlCommand(query, sqlConn))
+           {
+              sqlConn.Open();
+              // 2. define parameters used in command object
+              SqlParameter param = new SqlParameter();
+              param.ParameterName = "@Keyword";
+              param.Value = "%" + keyword + "%";
+              // 3. add new parameter to command object
+              cmd.Parameters.Add(param);
 
-                    query += "DateCreated BETWEEN '" + startDate.ToString("MM/dd/yyyy") + "' AND '" + endDate.ToString("MM/dd/yyyy") + " '";
-                }
-                if (supporterID != 0)
-                {
-                    if (counter > 0) query += " AND ";
-                    counter++;
-                    query += "Supporters.ID = " + supporterID;
-                }
-                if (typeID != 0)
-                {
-                    if (counter > 0) query += " AND ";
-                    counter++;
-                    query += "Types.ID = " + typeID;
-                }
-            }
-            using (SqlConnection sqlConn = new SqlConnection(ConnectionString))
-            using (SqlCommand cmd = new SqlCommand(query, sqlConn))
-            {
-                sqlConn.Open();
-                // 2. define parameters used in command object
-                SqlParameter param = new SqlParameter();
-                param.ParameterName = "@Keyword";
-                param.Value = "%"+keyword+"%";
-                // 3. add new parameter to command object
-                cmd.Parameters.Add(param);
-                
-                DataTable dt = new DataTable();
-                dt.Load(cmd.ExecuteReader());
-                return dt;
-            }
-            
-            
+              DataTable dt = new DataTable();
+              dt.Load(cmd.ExecuteReader());
+              return dt;
+           }
+
+
         }
     }
 }
